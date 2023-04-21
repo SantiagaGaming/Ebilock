03889aecc6f1743f6ea9af5289b8ecf5ffe17a54
Ilya <ganychev92@gmail.com>
2023-04-21 15:30:15 +0300

    NavMesh added

---
+++ Assets/Scripts/Other/Worker/WorkerController.cs
@@ -0,0 +1,45 @@
+using AosSdk.Core.PlayerModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class WorkerController : MonoBehaviour
+{
+    [SerializeField] private API _api;
+    [SerializeField] private Animator _anim;
+    [SerializeField] private Transform _playerPos;
+    private NavMeshAgent _navMeshAgent;
+    private bool _follow = false;
+    private void Awake()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+    private void Start()
+    {
+        _api.OnSetLocation += OnCheckState;
+    }
+    void Update()
+    {
+        if (!_follow)
+            return;
+        _navMeshAgent.SetDestination(_playerPos.position);
+        if(_navMeshAgent.velocity==Vector3.zero)
+            _anim.SetTrigger("Idle");
+        else
+            _anim.SetTrigger("Walk");
+    }
+
+    private void OnCheckState(string locationName)
+    {
+        if (locationName == "field")
+        {
+            _follow= true;
+        }
+        else if(locationName=="hall")
+        {
+            _follow = false;
+        }   
+    }
+}
